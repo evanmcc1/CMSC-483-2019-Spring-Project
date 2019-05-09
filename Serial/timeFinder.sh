cca77c1769ed1f4ade083bb5633b13dabe57a37d
Evan McCarter <evanmcc1@umbc.edu>
2019-05-09 19:01:41 -0400

    Remove 0m from times, increase max locations

--- Serial/timeFinder.sh
+++ Serial/timeFinder.sh
@@ -5,8 +5,8 @@ make routeFinder generateCity
 
 # Remove old data
 rm routeSettings.txt
-rm routeResults.txt
 rm routeOutput.txt
+rm routeResults.txt
 
 # Increment vehicles
 for i in {1..32}
@@ -14,14 +14,14 @@ do
 # Increment locations
 # RouteFinder crashes if there are fewer locations than vehicles
 # Ensure there are at least 3 locations
-for j in $( seq $(($i>3?$i:3)) 32 )
+for j in $( seq $(($i>3?$i:3)) 128 )
 do
 	echo "$i $j" | tee -a routeSettings.txt
 	{ time ./generateCity $j | ./routeFinder $i 0; } >> routeOutput.txt 2>> routeResults.txt;
 done
 done
 
-cat routeResults.txt | sed -e '/^real/d' -e '/^sys/d' -e '/^$/d' -e 's/user\t//' -e 's/s//' > routeTimes.txt
+cat routeResults.txt | sed -e '/^real/d' -e '/^sys/d' -e '/^$/d' -e 's/user\t//' -e 's/s//' -e 's/0m//' > routeTimes.txt
 cat routeOutput.txt | sed -e '/^S/!d' -e 's/Score: //' > routeScores.txt
 cat routeSettings.txt | awk '{ print $1 }' > routeVehicles.txt
 cat routeSettings.txt | awk '{ print $2 }' > routeLocations.txt
