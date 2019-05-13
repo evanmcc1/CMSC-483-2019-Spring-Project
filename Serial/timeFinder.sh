8840e46cedd158f86de4c97526dad3f012bdb1d7
Evan McCarter <evanmcc1@umbc.edu>
2019-05-13 16:45:28 -0400

    Find more statistics and add comments about them

--- Serial/timeFinder.sh
+++ Serial/timeFinder.sh
@@ -17,12 +17,17 @@ do
 for j in {1..256}
 do
 	echo "$i $j" | tee -a routeSettings.txt
-	{ time ./generateCity $j | ./routeFinder $i 0; } >> routeOutput.txt 2>> routeResults.txt;
+	{ time ./generateCity $j | ./routeFinder $i 0; } >> routeOutput.txt 2>> routeTimes.txt;
 done
 done
 
-cat routeResults.txt | sed -e '/^real/d' -e '/^sys/d' -e '/^$/d' -e 's/user\t//' -e 's/s//' -e 's/0m//' > routeTimes.txt
-cat routeOutput.txt | sed -e '/^S/!d' -e 's/Score: //' > routeScores.txt
+# Collect running times
+cat routeTimes.txt | sed -e '/^real/d' -e '/^sys/d' -e '/^$/d' -e 's/user\t//' -e 's/s//' -e 's/0m//' > routeUsertimes.txt
+cat routeTimes.txt | sed -e '/^real/d' -e '/^user/d' -e '/^$/d' -e 's/sys\t//' -e 's/s//' -e 's/0m//' > routeSystimes.txt
+# Collect total scores
+cat routeOutput.txt | sed -e '/^Overall score: /!d' -e 's/Overall score: //' > routeScores.txt
+# Collect average route length
+cat routeOutput.txt | sed -e '/^Average route length: /!d' -e 's/Average route length: //' > routeLengths.txt
+# Separate route settings per variable
 cat routeSettings.txt | awk '{ print $1 }' > routeVehicles.txt
 cat routeSettings.txt | awk '{ print $2 }' > routeLocations.txt
-# TODO: Collect average route length
